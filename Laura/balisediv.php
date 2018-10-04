00b501f4706dcaf96e63cb37b847a3f10cd24344
rainbow59 <laura.vangrevenynghe@laposte.net>
2018-10-04 20:21:31 +0200

    sauvegarde

---
+++ Laura/balisediv.php
@@ -0,0 +1,38 @@
+<?php include 'introheader.php';?>
+<title>Page type</title>
+<?php include 'header.php';?>
+
+    <div id="style_page">
+
+        <div class="gauche">
+            <!-- block gris foncé -->
+        </div>
+
+        <div class="contenu">
+            <div class="titres">
+                <h2>Balises Particulières : <span>balise &#8249;html&#8250;</span></h2>
+            </div>
+            <div class="def">
+                <p>Balise &#8249;div&#8250;&#8249;/div&#8250;</p>
+                <p>Cette balise dite balise universelle, définit une division. </p>
+                <p>type de balise (block ou inlines ou particulières)</p>
+                <p>type de balise (par paire ou orphelines)</p>
+                <p>où et quand dans le code (body...)</p>
+                <p>sa fonction</p>
+                <p>Décliner les attributs (nécessaires et optionels")</p>
+                <br>
+                <p id="txtavimg">Voici un exemple de la balise utilisée dans un script html :</p>
+                <img class="capexemple" src="capturew3cmeta.PNG" alt="image provenant de w3c">
+                <br>
+                <a class="liensites" href="http://www.monsite.fr" target="http://www.monsite.fr" onclick="www.monsite.fr">mon site</a>
+                <br>
+            </div>
+
+            <form class ="boutonsuivant" action="balisehtml.html">
+                <input type="submit" value="Suivant" />
+            </form>
+
+        </div>
+    </div>
+
+    <?php include 'footer.php';?>
