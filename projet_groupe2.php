bb821953e0723a73f3d6752ddc6c4115419c30b7
IvoryAndSmalt <lucas180395@gmail.com>
2018-10-04 13:34:52 +0200

    c la vi

--- projet_groupe2.php
+++ projet_groupe2.php
@@ -8,46 +8,8 @@
 </head>
 
 <body>
-    <header>
-        <h1>INITIATION AU HTML</h1>
-
-        <ul>
-            <li><a href="projet_groupe2.html">Accueil </a></li>
-            <li><a>Balises Block</a>
-                <ul>
-                    <li><a href="balisediv.html">&#8249;div&#8250;</a></li>
-                    <li><a href="baliseul.html">&#8249;ul&#8250;</a></li>
-                    <li><a href="baliseli.html">&#8249;li&#8250;</a></li>
-                    <li><a href="balisep.html">&#8249;p&#8250;</a></li>
-                    <li><a href="baliseheader.html">&#8249;header&#8250;</a></li>
-                    <li><a href="balisefooter.html">&#8249;footer&#8250;</a></li>
-                    <li><a href="balisevideo.html">&#8249;video&#8250;</a></li>
-                </ul>
-            </li>
-            <li><a>Balises Inline</a>
-                <ul>
-                    <li><a href="balisea.html">&#8249;a&#8250;</a></li>
-                    <li><a href="baliseimg.html">&#8249;img&#8250;</a></li>
-                    <li><a href="balisebutton.html">&#8249;button&#8250;</a></li>
-                    <li><a href="balisetextarea.html">&#8249;textarea&#8250;</a></li>
-                </ul>
-            </li>
-            <li><a>Balises Particulières</a>
-                <ul>
-                    <li><a href="balisehtml.html">&#8249;html&#8250;</a></li>
-                    <li><a href="balisemeta.html">&#8249;meta&#8250;</a></li>
-                    <li><a href="balisehead.html">&#8249;head&#8250;</a></li>
-                    <li><a href="balisea.html">&#8249;body&#8250;</a></li>
-                </ul>
-            </li>
-            <li><a>Contact</a>
-                <ul>
-                    <li><a href="contactform.html" target="contactform.html" onclick="contactform.html">Formulaire de
-                            Contact</a></li>
-                </ul>
-            </li>
-        </ul>
-    </header>
+    
+<?php include ('header.php') ; ?>
 
     <div id="style_page">
 
