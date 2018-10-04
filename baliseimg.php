696869c344813e9e40241be5f487c53e75fc220d
IvoryAndSmalt <lucas180395@gmail.com>
2018-10-04 17:03:10 +0200

    soir

--- baliseimg.php
+++ baliseimg.php
@@ -1,5 +1,5 @@
 <?php include 'introheader.php';?>
-<title>Introduction au html : balise video</title>
+<title>Introduction au html : balise img</title>
 <?php include 'header.php';?>
 
     <div id="style_page">
@@ -10,15 +10,19 @@
 
         <div class="contenu">
             <div class="titres">
-                <h2>Balises Particulières : <span>balise &#8249;video&#8250;</span></h2>
+                <h2>Balises Particulières : <span>balise &#8249;img&#8250;</span></h2>
             </div>
             <div class="def">
-                <p>nom de la balise &#8249;video&#8250;</p>
-                <p>La balise video est une balise block.</p>
-                <p>Elle est composée d'une balise entrante et sortante</p>
+                <p>nom de la balise &#8249;img&#8250;</p>
+                <p>La balise video est une balise inline.</p>
+                <p>C'est une balise orpheline.</p>
                 <p>Elle est située dans le body du document html.</p>
-                <p>La balise video permet d'intégrer un fichier video à du code html. 
-                    Actuellement, les formats video supportés sont mp4, WebM et ogg.
+                <p>La balise img permet d'intégrer une image à du code html. Tous les navigateurs 
+                    ne supportent pas nécessairement les mêmes formats d'image. Voici la liste les 
+                    formats supportés par les navigateurs principaux.
+                    <a href="https://en.wikipedia.org/wiki/Comparison_of_web_browsers#Image_format_support" 
+                    target="https://en.wikipedia.org/wiki/Comparison_of_web_browsers#Image_format_support"
+                    onclick="https://en.wikipedia.org/wiki/Comparison_of_web_browsers#Image_format_support"></a>
                 </p>
                 <p>De la même manière que la balise img, il faut utiliser l'attribut src="" 
                     pour spécifier la source. les attributs width et height sont souvent
