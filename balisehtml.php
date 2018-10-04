aa7fba0ee24c0eb80753d3309a16b73737cb17d3
IvoryAndSmalt <lucas180395@gmail.com>
2018-10-04 16:10:05 +0200

    ajout balise beta et images

--- balisehtml.php
+++ balisehtml.php
@@ -1,5 +1,5 @@
 <?php include 'introheader.php';?>
-<title>Introduction au html : balise meta</title>
+<title>Introduction au html : balise html</title>
 <?php include 'header.php';?>
 
     <div id="style_page">
@@ -10,26 +10,23 @@
 
         <div class="contenu">
             <div class="titres">
-                <h2>Balises Particulières : <span>balise &#8249;meta&#8250;</span></h2>
+                <h2>Balises Particulières : <span>balise &#8249;html&#8250;</span></h2>
             </div>
             <div class="def">
-                <p>nom de la balise &#8249;meta&#8250;</p>
-                <p>"meta" vient de metadata (métadonnées en français)</p>
+                <p>nom de la balise &#8249;html&#8250;</p>
+                <p>HTML signifie HyperText Markup Language</p>
                 <p>Il s'agit d'une balise particulière, pas tout à fait un block ou inline.</p>
-                <p>C'est une balise orpheline, donc sans balise sortante.</p>
-                <p>Elle se situe dans la balise &#8249;head&#8250;</p>
-                <p>La balise meta renseigne les métadonnées du document html qui ne sont pas déjà
-                    définies par d'autres balises, comme &#8249;title&#8250;. Elle sert à renseigner
-                    les moteurs de recherche et les navigateurs sur comment utiliser la page web.
+                <p>Elle est composée d'une balise entrante et sortante</p>
+                <p>Elle est située en-dessous de la définition du DOCTYPE, et englobe ‹head› et ‹body›</p>
+                <p>La balise ‹html› est la racine d'un document html. les autres balises seront donc descendantes de celle-ci. 
+                    Elle sert à délimiter le document html.
                 </p>
-                <p>les attributs name="description" content="contenu", ou encore charset="utf-8" sont
-                    des attributs fréquemment utilisés. <br>Pour une liste plus exhaustive, cliquez 
-                    <a href="https://gist.github.com/kevinSuttle/1997924" target="https://gist.github.com/kevinSuttle/1997924"
-                    class="liengithubmeta">ici</a>.
+                <p>Aucun attribut n'est nécessaire à cette balise. Elle supporte toutefois tous les attributs universels on lui 
+                    attribue habituellement l'attribut "lang". exemple : ‹html lang="fr"› 
                 </p>
                 <br>
                 <p id="txtavimg">Voici un exemple de la balise utilisée dans un script html :</p>
-                <img class="capexemple" src="capturew3cmeta.PNG" alt="image provenant de w3c">
+                <img class="capexemple" src="Capturew3cbalisehtml.PNG" alt="image provenant de w3c">
                 <br>
                 <a class="liensites" href="https://www.w3schools.com/tags/tag_meta.asp" target="https://www.w3schools.com/tags/tag_meta.asp" onclick="https://www.w3schools.com/tags/tag_meta.asp">w3schools.com/tags/tag_meta.asp</a>
                 <a class="liensites" href="https://developer.mozilla.org/fr/docs/Web/HTML/Element/meta" target="https://developer.mozilla.org/fr/docs/Web/HTML/Element/meta" onclick="https://developer.mozilla.org/fr/docs/Web/HTML/Element/meta">developer.mozilla.org/fr/docs/Web/HTML/Element/meta</a>
@@ -39,7 +36,6 @@
             <form class ="boutonsuivant" action="balisehead.php">
                 <input type="submit" value="Suivant" />
             </form>
-
         </div>
     </div>
 
