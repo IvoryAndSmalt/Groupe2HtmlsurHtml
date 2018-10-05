71b8031445715391e8984bf799fc017b39b6bf02
rainbow59 <laura.vangrevenynghe@laposte.net>
2018-10-05 11:52:55 +0200

    check

--- accueil.php
+++ accueil.php
@@ -10,14 +10,29 @@
         </div>
 
         <div class="sujet">
-            <h2>HTML SUR HTML</h2>
+            <h2><span class="attributciter">Les balises en paires &#8249;&#8250;&#8249;/&#8250;</span> </h2>
             <br>
-            <p>
-                Utilisation des notions vues pour créer un mini site cours de html.
-                Aucun CSS utilisé, mise en forme brute html.
-                Chaque étudiant choisit au moins 4 balises html, doit la présenter et mettre en forme sa présentation.
-                Intégration d’un menu commun (mode list UL/LI).
-            </p>
+            <p> Elles servent à délimiter un début et une fin. Ce sont des balises qui s’ouvrent et se ferment.</p>
+            <br>
+            <h2><span class="attributciter">Les balises orphelines &#8249;/&#8250; </span></h2>
+            <br>
+            <p> Elles servent à délimiter un début et une fin. Ce sont des balises qui s’ouvrent et se ferment.</p>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <h2><span class="attributciter">Les balises block </span></h2>
+            <br>
+            <p> Elles sont utilisées pour regrouper des éléments sous forme de bloc et ainsi pouvoir les modifier en CSS (Cascading Style Sheets ou feuille de style). Elles provoquent obligatoirement un retour à la ligne.</p>
+            <br>
+            <h2><span class="attributciter">Les balises inline </span></h2>
+            <br>
+            <p>Signifie en ligne. Elles se trouvent obligatoirement dans une balise block et elles ne créent pas de retour à la ligne.</p>
+            <br>
+            <h2><span class="attributciter">Les balises particulières </span></h2>
+            <br>
+            <p> Elles apportent des informations sur la struture sémantique de la page html. Elles agissent comme des balises de type block.</p>
+            <br/>
         </div>
     </div>
 
